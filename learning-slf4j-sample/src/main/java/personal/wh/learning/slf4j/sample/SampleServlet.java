c447988044804fa1e1d02125a321bc4d120d0480
wanghui <wanghui.tj.1987@gmail.com>
2017-11-26 18:09:15 +0800

    add learning-slf4j-sample

---
+++ learning-slf4j-sample/src/main/java/personal/wh/learning/slf4j/sample/SampleServlet.java
@@ -0,0 +1,29 @@
+package personal.wh.learning.slf4j.sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SampleServlet extends HttpServlet {
+
+    private static final long serialVersionUID = -870284320856607145L;
+    
+    private static final Logger logger = LoggerFactory.getLogger(SampleServlet.class);
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.debug("Hello Slf4j ! - debug");
+        logger.info("Hello Slf4j ! - info");
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.write("ok");
+        }
+    }
+    
+}
